e32cc3434dfcc022ed7c273a3e11c499487108e1
De Ville Weppenaar <dweppenaar@gmail.com>
2020-06-03 17:57:59 -0700

    add remove function

--- binary-search-tree/bst.go
+++ binary-search-tree/bst.go
@@ -55,6 +55,19 @@ func (t *Tree) String() string {
 	return fmt.Sprintf("%s", t.root)
 }
 
+// Equal returns true if the structure matches the provided tree.
+func (t *Tree) Equal(tree *Tree) bool {
+	if t.root == nil && tree.root == nil {
+		return true
+	}
+
+	if t.root == nil || tree.root == nil {
+		return false
+	}
+
+	return t.root.Equal(tree.root)
+}
+
 // Add a node to the tree.
 // Returns true when the node could be added.
 func (t *Tree) Add(value int) bool {
@@ -92,19 +105,89 @@ func (t *Tree) Add(value int) bool {
 // Remove a node from the tree.
 // Returns true when the node could be deleted.
 func (t *Tree) Remove(value int) bool {
+	var parent *Node
+	current := t.root
+	for {
+		if current == nil {
+			return false
+		}
 
-	return false
-}
+		if current.value == value {
+			break
+		}
 
-// Equal returns true if the structure matches the provided tree.
-func (t *Tree) Equal(tree *Tree) bool {
-	if t.root == nil && tree.root == nil {
+		parent = current
+		if value < current.value {
+			current = current.left
+			continue
+		}
+		current = current.right
+	}
+
+	switch {
+	case current.left == nil && current.right == nil:
+		// leaf node
+		if parent == nil {
+			// root leaf node
+			t.root = nil
+			return true
+		}
+
+		if parent.left == current {
+			parent.left = nil
+		} else {
+			parent.right = nil
+		}
 		return true
-	}
+	case current.left != nil && current.right == nil:
+		// only has left child
+		if parent == nil {
+			t.root = current.left
+			return true
+		}
 
-	if t.root == nil || tree.root == nil {
-		return false
-	}
+		if parent.left == current {
+			parent.left = current.left
+		} else {
+			parent.right = current.left
+		}
+		return true
+	case current.right != nil && current.left == nil:
+		// only has right child
+		if parent == nil {
+			t.root = current.right
+			return true
+		}
 
-	return t.root.Equal(tree.root)
+		if parent.left == current {
+			parent.left = current.right
+		} else {
+			parent.right = current.right
+		}
+		return true
+	default:
+		// has two children
+		right := current.right
+		left := current.left
+
+		if parent.left == current {
+			parent.left = right
+		} else {
+			parent.right = right
+		}
+
+		// find place for left sub tree
+		currLeft := right
+		var currLeftParent *Node
+		for {
+			if currLeft == nil {
+				// found space
+				currLeftParent.left = left
+				return true
+			}
+
+			currLeftParent = currLeft
+			currLeft = currLeft.left
+		}
+	}
 }
--- binary-search-tree/bst_test.go
+++ binary-search-tree/bst_test.go
@@ -166,5 +166,126 @@ func TestAdd(t *testing.T) {
 }
 
 func TestRemove(t *testing.T) {
-	t.Error("Implement Remove function.")
+	tests := []struct {
+		desc    string
+		tree    *Tree
+		value   int
+		want    *Tree
+		removed bool
+	}{
+		{
+			"empty",
+			&Tree{},
+			5,
+			&Tree{},
+			false,
+		},
+		{
+			"root",
+			&Tree{root: &Node{value: 5}},
+			5,
+			&Tree{},
+			true,
+		},
+		{
+			"root not found",
+			&Tree{root: &Node{value: 5}},
+			3,
+			&Tree{root: &Node{value: 5}},
+			false,
+		},
+		{
+			"root node, left",
+			&Tree{root: &Node{value: 5, left: &Node{value: 2}}},
+			5,
+			&Tree{root: &Node{value: 2}},
+			true,
+		},
+		{
+			"root node, right",
+			&Tree{root: &Node{value: 5, right: &Node{value: 8}}},
+			5,
+			&Tree{root: &Node{value: 8}},
+			true,
+		},
+		{
+			"leaf left, left",
+			&Tree{root: &Node{value: 5, left: &Node{value: 2, left: &Node{value: 1}}, right: &Node{value: 7}}},
+			1,
+			&Tree{root: &Node{value: 5, left: &Node{value: 2}, right: &Node{value: 7}}},
+			true,
+		},
+		{
+			"leaf left, right",
+			&Tree{root: &Node{value: 5, left: &Node{value: 2, right: &Node{value: 3}}, right: &Node{value: 7}}},
+			3,
+			&Tree{root: &Node{value: 5, left: &Node{value: 2}, right: &Node{value: 7}}},
+			true,
+		},
+		{
+			"leaf right, left",
+			&Tree{root: &Node{value: 5, left: &Node{value: 2}, right: &Node{value: 7, left: &Node{value: 6}}}},
+			6,
+			&Tree{root: &Node{value: 5, left: &Node{value: 2}, right: &Node{value: 7}}},
+			true,
+		},
+		{
+			"leaf right, right",
+			&Tree{root: &Node{value: 5, left: &Node{value: 2}, right: &Node{value: 7, right: &Node{value: 8}}}},
+			8,
+			&Tree{root: &Node{value: 5, left: &Node{value: 2}, right: &Node{value: 7}}},
+			true,
+		},
+		{
+			"one child, left",
+			&Tree{root: &Node{value: 5, left: &Node{value: 2}, right: &Node{value: 7, left: &Node{value: 6}}}},
+			7,
+			&Tree{root: &Node{value: 5, left: &Node{value: 2}, right: &Node{value: 6}}},
+			true,
+		},
+		{
+			"one child, right",
+			&Tree{root: &Node{value: 5, left: &Node{value: 2}, right: &Node{value: 7, right: &Node{value: 8}}}},
+			7,
+			&Tree{root: &Node{value: 5, left: &Node{value: 2}, right: &Node{value: 8}}},
+			true,
+		},
+		{
+			"two children, left",
+			&Tree{root: &Node{value: 5, left: &Node{value: 2}, right: &Node{value: 7, left: &Node{value: 6}, right: &Node{value: 8}}}},
+			7,
+			&Tree{root: &Node{value: 5, left: &Node{value: 2}, right: &Node{value: 8, left: &Node{value: 6}}}},
+			true,
+		},
+		{
+			"two children, right",
+			&Tree{root: &Node{value: 8, left: &Node{value: 4, left: &Node{value: 1}, right: &Node{value: 7}}, right: &Node{value: 12}}},
+			4,
+			&Tree{root: &Node{value: 8, left: &Node{value: 7, left: &Node{value: 1}}, right: &Node{value: 12}}},
+			true,
+		},
+		{
+			"nested not found",
+			&Tree{root: &Node{value: 8, left: &Node{value: 4, left: &Node{value: 1}, right: &Node{value: 7}}, right: &Node{value: 7, left: &Node{value: 6}, right: &Node{value: 8}}}},
+			9,
+			&Tree{root: &Node{value: 8, left: &Node{value: 4, left: &Node{value: 1}, right: &Node{value: 7}}, right: &Node{value: 7, left: &Node{value: 6}, right: &Node{value: 8}}}},
+			false,
+		},
+	}
+
+	for _, test := range tests {
+		gr := test.tree.Remove(test.value)
+		wr := test.removed
+
+		if gr != wr {
+			t.Errorf("%v: got removed %t, want removed %t", test.desc, gr, wr)
+			continue
+		}
+
+		gt := test.tree
+		wt := test.want
+		if !gt.Equal(wt) {
+			t.Errorf("%v:\ngot\t%s\nwant\t%s", test.desc, gt, wt)
+		}
+	}
 }
